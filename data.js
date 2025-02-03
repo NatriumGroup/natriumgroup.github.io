09f56b8291d7cf28ec9973e74220243bc3826bfe
Weiqu_Diamond_Block <83117490+diamond-weiqu@users.noreply.github.com>
2025-02-03 19:45:13 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const blacklistData = [
+    { 
+        qq: ['123456789', '987654321'],  // 多个 QQ
+        qqNickname: ['张三的QQ', '张三第二号'],  // 多个 QQ 昵称
+        name: ['张三'],  // 姓名也支持多个
+        phone: ['13812345678', '13987654321'],  // 多个手机号
+        alipay: ['zhangsan@alipay.com'],  // 支付宝账号
+        wechat: ['zhangsan_wechat', 'zhangsan_wechat2'],  // 多个微信账号
+        bankCard: ['6222020202012345', '6222020202012346'],  // 多个银行卡
+        idCard: ['110101199003078739']  // 身份证也支持多个
+    },
+    { 
+        qq: ['223344556', '998877665'], 
+        qqNickname: ['李四的QQ'],
+        name: ['李四'],
+        phone: ['15012345678', '15987654321'], 
+        alipay: ['lisi@alipay.com'], 
+        wechat: ['lisi_wechat'],
+        bankCard: ['6222020202023456'], 
+        idCard: ['110102199503078739'] 
+    },
+    // 更多数据...
+];
