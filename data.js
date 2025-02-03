e2d99a17710486adea7b36c8aeea5a23f542b5d7
Weiqu_Diamond_Block <83117490+diamond-weiqu@users.noreply.github.com>
2025-02-03 20:53:27 +0800

    Update data.js

--- data.js
+++ data.js
@@ -49,4 +49,14 @@ const blacklistData = [
         bankCard: [''],  // 多个银行卡
         idCard: ['']  // 身份证也支持多个
     },
+    { 
+        qq: [''],  // 多个 QQ
+        qqNickname: ['阿里聚合支付'],  // 多个 QQ 昵称
+        name: ['张旭东'],  // 姓名也支持多个
+        phone: [''],  // 多个手机号
+        alipay: [''],  // 支付宝账号
+        wechat: [''],  // 多个微信账号
+        bankCard: [''],  // 多个银行卡
+        idCard: ['']  // 身份证也支持多个
+    },
 ];
