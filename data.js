de288e080b5cf4d9320b2dae4f28729b5a89d824
Weiqu_Diamond_Block <83117490+diamond-weiqu@users.noreply.github.com>
2025-02-03 20:00:02 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 const blacklistData = [
     { 
         qq: ['2175709646', '276423602'],  // 多个 QQ
-        qqNickname: ['夜白/世俗', '龙儿'],  // 多个 QQ 昵称
-        name: [''],  // 姓名也支持多个
+        qqNickname: ['世俗', '龙儿'],  // 多个 QQ 昵称
+        name: ['夜白'],  // 姓名也支持多个
         phone: [''],  // 多个手机号
         alipay: [''],  // 支付宝账号
         wechat: [''],  // 多个微信账号
