4cf4b5c4b3c17139bbdca1c932dfe93ae3761a77
Weiqu_Diamond_Block <83117490+diamond-weiqu@users.noreply.github.com>
2025-02-03 20:33:07 +0800

    Update data.js

--- data.js
+++ data.js
@@ -19,7 +19,7 @@ const blacklistData = [
         bankCard: [''], 
         idCard: [''] 
     },
-        { 
+    { 
         qq: ['5511984','3784621615','857350111','3177794450','1425228431'], 
         qqNickname: ['杀神/诗诗','时间/财富','黑狼','北念','糖豆'],
         name: ['崔艳兰'],
@@ -28,6 +28,16 @@ const blacklistData = [
         wechat: [''],
         bankCard: [''], 
         idCard: [''] 
+    },
+    { 
+        qq: ['2454928288'],  // 多个 QQ
+        qqNickname: ['猩猩'],  // 多个 QQ 昵称
+        name: [''],  // 姓名也支持多个
+        phone: [''],  // 多个手机号
+        alipay: [''],  // 支付宝账号
+        wechat: [''],  // 多个微信账号
+        bankCard: [''],  // 多个银行卡
+        idCard: ['']  // 身份证也支持多个
     },
     { 
         qq: ['1535959373'],  // 多个 QQ
